8b0617b160979df68bfb6ec022a287190173fce9
Luis Felipe <stefanluis78@gmail.com>
2024-06-05 16:52:44 -0300

    Create SellerDaoJDBC and DaoFactory class

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.time.LocalDate;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,9 @@ public class Program {
 		
 		Department dp = new Department(1,"Books");
 		Seller seller = new Seller(1,"Pedro","pedro@gmail.com", LocalDate.now(), 3000.0, dp);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		System.out.println(seller);
 
 	}
